21cd498a8d3dfc58bc3d7a390aba6268bf35b8c8
ybathula707 <60422402+ybathula707@users.noreply.github.com>
2024-01-18 23:46:10 -0800

    barbie-fied

--- server.py
+++ server.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/hello_barbie")
+def hello_barbie():
+    return render_template('index_barbie.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')    
